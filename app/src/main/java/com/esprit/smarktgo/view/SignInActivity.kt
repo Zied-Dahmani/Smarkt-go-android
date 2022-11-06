e74a966192a77459b0aba18c95e8aaa2f61aae96
Mohamed Islem Samaali <74082867+myrmn@users.noreply.github.com>
2022-11-07 00:46:06 +0100

    phone auth

--- app/src/main/java/com/esprit/smarktgo/view/SignInActivity.kt
+++ app/src/main/java/com/esprit/smarktgo/view/SignInActivity.kt
@@ -15,6 +15,12 @@ import com.esprit.smarktgo.viewmodel.SignInViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 
 const val RC_SIGN_IN = 1001
@@ -24,14 +30,27 @@ class SignInActivity : AppCompatActivity() {
     lateinit var signInViewModel : SignInViewModel
     private lateinit var binding : ActivitySignInBinding
     val loading = LoadingDialog(this)
+    lateinit var auth: FirebaseAuth
+    var number: String = ""
+    lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // create instance of firebase auth
+
+        // we will use this to match the sent otp from firebase
+        lateinit var storedVerificationId: String
+        lateinit var resendToken: PhoneAuthProvider.ForceResendingToken
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
 
-        signInViewModel= SignInViewModel(this)
+        // this stores the phone number of the user
+
+
+        signInViewModel = SignInViewModel(this)
 
         binding.googleButton.setOnClickListener {
             signIn()
@@ -39,8 +58,59 @@ class SignInActivity : AppCompatActivity() {
 
         binding.phoneButton.setOnClickListener { signInViewModel.signOut() }
 
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            // This method is called when the verification is completed
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
+                Log.d("GFG", "onVerificationCompleted Success")
+            }
+
+            // Called when verification is failed add log statement to see the exception
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.d("GFG", "onVerificationFailed  $e")
+            }
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                Log.d("GFG","onCodeSent: $verificationId")
+                storedVerificationId = verificationId
+                resendToken = token
+
+                // Start a new activity using intent
+                // also send the storedVerificationId using intent
+                // we will use this id to send the otp back to firebase
+                val intent = Intent(applicationContext,SignInViewModel::class.java)
+                intent.putExtra("storedVerificationId",storedVerificationId)
+                startActivity(intent)
+                finish()
+            }
+
+        }
     }
 
+    private fun login() {
+ number=binding.phone.text?.trim().toString()
+        // get the phone number from edit text and append the country cde with it
+        if (number.isNotEmpty()){
+            number = "+216$number"
+            sendVerificationCode(number)
+        }else{
+            Toast.makeText(this,"Enter mobile number", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun sendVerificationCode(number: String) {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(number) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this) // Activity (for callback binding)
+            .setCallbacks(callbacks) // OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        Log.d("GFG" , "Auth started")
+    }
     private fun signIn() {
         val signInIntent = signInViewModel.mGoogleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
