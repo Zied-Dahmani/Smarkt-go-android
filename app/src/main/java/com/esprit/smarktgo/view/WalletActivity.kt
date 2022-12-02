2a88798a6f7bf1573ee8900142ee88863a3c3cac
Zied Dahmani <zied.dahmani2@gmail.com>
2022-12-02 13:34:29 +0100

    Scan init

--- app/src/main/java/com/esprit/smarktgo/view/WalletActivity.kt
+++ app/src/main/java/com/esprit/smarktgo/view/WalletActivity.kt
@@ -4,33 +4,21 @@ package com.esprit.smarktgo.view
 import android.Manifest
 import android.app.ProgressDialog
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.view.Menu
-import android.widget.PopupMenu
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.esprit.smarktgo.R
 import com.esprit.smarktgo.databinding.ActivityWalletBinding
+import com.esprit.smarktgo.viewmodel.CAMERA_REQUEST_CODE
+import com.esprit.smarktgo.viewmodel.STORAGE_REQUEST_CODE
 import com.esprit.smarktgo.viewmodel.WalletViewModel
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.TextRecognizer
-import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-
 
 class WalletActivity : AppCompatActivity() {
-    var CAMERA_REQUEST_CODE = 100
-    var STORAGE_REQUEST_CODE = 101
-    private lateinit var cameraPermissions: Array<String>
-    private lateinit var storagePermissions: Array<String>
-    private lateinit var progressDialog: ProgressDialog
-    private lateinit var textRecognizer: TextRecognizer
 
-    private lateinit var binding: ActivityWalletBinding
+    lateinit var binding: ActivityWalletBinding
     lateinit var walletViewModel: WalletViewModel
     lateinit var id: String
     var wallet: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wallet)
@@ -47,73 +35,21 @@ class WalletActivity : AppCompatActivity() {
         binding.fillButton.setOnClickListener {
             walletViewModel.fill(binding.code.text.toString())
         }
-        cameraPermissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        storagePermissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-        progressDialog = ProgressDialog(this)
-        progressDialog.setTitle("Please wait")
-        progressDialog.setCanceledOnTouchOutside(false)
-        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
         binding.Pick.setOnClickListener {
-            showInputImageDialog()
-
+            walletViewModel.showInputImageDialog()
         }
+
         binding.Scan.setOnClickListener {
            if ( walletViewModel.imageUri == null) {
-                showToast("PICK IMAGE FIRST")
+               walletViewModel.showToast("PICK IMAGE FIRST")
             } else {
-                recognizeTextFromImage()
+               walletViewModel.recognizeTextFromImage()
             }
         }
     }
 
-    private fun recognizeTextFromImage() {
-        progressDialog.setMessage("Preparing Image...")
-        progressDialog.show()
-        try {
-            val inputImage = InputImage.fromFilePath(this,walletViewModel.imageUri!!)
-            progressDialog.setMessage("Recognizing text")
-            val textTaskResult = textRecognizer.process(inputImage)
-                .addOnSuccessListener { text ->
-                    progressDialog.dismiss()
-                    val recognizedText = text.text
-                    binding.code.setText(recognizedText)
-                }
-                .addOnFailureListener { e ->
-                    progressDialog.dismiss()
-                    showToast("Failed to recognize text due to ${e.message}")
-                }
-        } catch (e: Exception) {
-            progressDialog.dismiss()
-            showToast("Failed to prepare image due to ${e.message}")
-        }
-    }
 
-    private fun showInputImageDialog() {
-
-        val popupMenu = PopupMenu(this, binding.Scan)
-        popupMenu.menu.add(Menu.NONE, 1, 1, "CAMERA")
-        popupMenu.menu.add(Menu.NONE, 2, 2, "GALLERY")
-        popupMenu.show()
-        popupMenu.setOnMenuItemClickListener { menuItem ->
-            val id = menuItem.itemId
-            if (id == 1) {
-                if (walletViewModel.checkCameraPermission()) {
-                    walletViewModel.pickImageCamera()
-                } else {
-                    walletViewModel.requestCameraPermission()
-                }
-            } else if (id == 2) {
-                if (walletViewModel.checkStoragePermission()) {
-                    walletViewModel.pickImageGallery()
-                } else {
-                    walletViewModel.requestStoragePermission()
-                }
-            }
-            return@setOnMenuItemClickListener true
-        }
-    }
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -130,7 +66,7 @@ class WalletActivity : AppCompatActivity() {
                     if (cameraAccepted && storageAccepted) {
                         walletViewModel.pickImageCamera()
                     } else {
-                        showToast("CAMERA PERMISSION REQUIRED")
+                        walletViewModel.showToast("CAMERA PERMISSION REQUIRED")
                     }
                 }
             }
@@ -141,19 +77,13 @@ class WalletActivity : AppCompatActivity() {
                         walletViewModel.pickImageGallery()
 
                     } else {
-                        showToast("STORAGE PERMISSION IS REQUIRED")
+                        walletViewModel.showToast("STORAGE PERMISSION IS REQUIRED")
                     }
                 }
             }
         }
     }
 
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
 
-    fun showError(errorText: String) {
-        binding.codeContainer.error = errorText
-    }
 
 }
--- app/src/main/java/com/esprit/smarktgo/viewmodel/WalletViewModel.kt
+++ app/src/main/java/com/esprit/smarktgo/viewmodel/WalletViewModel.kt
@@ -2,11 +2,15 @@ package com.esprit.smarktgo.viewmodel
 
 import android.Manifest
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.MediaStore
+import android.util.Log
+import android.view.Menu
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
@@ -19,28 +23,47 @@ import com.esprit.smarktgo.R
 import com.esprit.smarktgo.model.UpdateTicket
 import com.esprit.smarktgo.repository.TicketRepository
 import com.esprit.smarktgo.view.WalletActivity
+import com.google.mlkit.vision.common.InputImage
 import kotlinx.coroutines.launch
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.TextRecognizer
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+
+const val CAMERA_REQUEST_CODE = 100
+const val STORAGE_REQUEST_CODE = 101
 
 class WalletViewModel(mActivity: WalletActivity): ViewModel() {
 
     private val mActivity = mActivity
-    var CAMERA_REQUEST_CODE = 100
-    var STORAGE_REQUEST_CODE = 101
-     var imageUri: Uri?=null
-    private lateinit var cameraPermissions:Array<String>
-    private lateinit var storagePermissions:Array<String>
+
+    var imageUri: Uri?=null
+    var cameraPermissions:Array<String>
+    var storagePermissions:Array<String>
     private val ticketRepository: TicketRepository = TicketRepository()
+    private var progressDialog: ProgressDialog
+    private val textRecognizer: TextRecognizer
+
+    init{
+        cameraPermissions = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        storagePermissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        progressDialog = ProgressDialog(mActivity)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setCanceledOnTouchOutside(false)
+        textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    }
+
 
 
     fun fill(code: String) : Boolean {
         if(code.isEmpty())
         {
-            mActivity.showError(mActivity.getString(R.string.type_the_ticket_code))
+            showError(mActivity.getString(R.string.type_the_ticket_code))
             return false
         }
         else if(code.length!=6)
         {
-            mActivity.showError(mActivity.getString(R.string.type_a_valid_code))
+            showError(mActivity.getString(R.string.type_a_valid_code))
             return false
         }
         else{
@@ -58,13 +81,13 @@ class WalletViewModel(mActivity: WalletActivity): ViewModel() {
                         }
                         else if(code==ticket.code.toString())
                         {
-                            mActivity.showError(mActivity.getString(R.string.invalid_ticket))
+                            showError(mActivity.getString(R.string.invalid_ticket))
                             found = true
                             break
                         }
                     }
                     if(!found)
-                        mActivity.showError(mActivity.getString(R.string.ticket_not_found))
+                        showError(mActivity.getString(R.string.ticket_not_found))
                 }
             }
         }
@@ -81,44 +104,54 @@ class WalletViewModel(mActivity: WalletActivity): ViewModel() {
             }
         }
     }
-     fun checkStoragePermission():Boolean {
-        return  ContextCompat.checkSelfPermission(mActivity,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED
+
+    fun showToast(message:String){
+        Toast.makeText(mActivity,message, Toast.LENGTH_SHORT).show()
     }
-     fun checkCameraPermission():Boolean {
+
+    private fun showError(errorText: String) {
+        mActivity.binding.codeContainer.error = errorText
+    }
+
+
+     private fun checkGalleryPermission():Boolean =  ContextCompat.checkSelfPermission(mActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED
+
+     private fun checkCameraPermission():Boolean {
         val storagePermission =   ContextCompat.checkSelfPermission(mActivity,
             Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED
         val cameraPermission= ContextCompat.checkSelfPermission(mActivity,
             Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED
-
         return storagePermission && cameraPermission
     }
-     fun requestStoragePermission(){
+
+     private fun requestGalleryPermission(){
         ActivityCompat.requestPermissions(mActivity,storagePermissions,STORAGE_REQUEST_CODE)
     }
-     fun requestCameraPermission(){
+     private fun requestCameraPermission(){
         ActivityCompat.requestPermissions(mActivity,cameraPermissions,CAMERA_REQUEST_CODE)
     }
+
      fun pickImageGallery(){
         val intent= Intent(Intent.ACTION_PICK)
         intent.type="image/*"
         galleryActivityResultLauncher.launch(intent)
+
+         //condition
+         //recognizeTextFromImage()
     }
 
      fun pickImageCamera(){
         val values= ContentValues()
         values.put(MediaStore.Images.Media.TITLE,"SIMPLE")
         values.put(MediaStore.Images.Media.DESCRIPTION,"DESCRIPTION")
-
         imageUri=mActivity.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
-
         val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri)
         cameraActivityResultLauncher.launch(intent)
     }
+
     private val cameraActivityResultLauncher =
         mActivity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-
             if (result.resultCode == Activity.RESULT_OK) {
 
             }
@@ -128,9 +161,7 @@ class WalletViewModel(mActivity: WalletActivity): ViewModel() {
         }
 
     private val galleryActivityResultLauncher =
-        mActivity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-                result->
-
+        mActivity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result->
             if (result.resultCode== Activity.RESULT_OK)
             {
                 val data=result.data
@@ -140,15 +171,54 @@ class WalletViewModel(mActivity: WalletActivity): ViewModel() {
             else {
                 showToast("Cancelled...")
             }
-
         }
 
-
-
-    private fun showToast(message:String){
-       Toast.makeText(mActivity,message, Toast.LENGTH_SHORT).show()
+    fun recognizeTextFromImage() {
+        progressDialog.setMessage("Preparing Image...")
+        progressDialog.show()
+        try {
+            val inputImage = InputImage.fromFilePath(mActivity,imageUri!!)
+            progressDialog.setMessage("Recognizing text")
+            val textTaskResult = textRecognizer.process(inputImage)
+                .addOnSuccessListener { text ->
+                    progressDialog.dismiss()
+                    val recognizedText = text.text
+                    mActivity.binding.code.setText(recognizedText)
+                }
+                .addOnFailureListener { e ->
+                    progressDialog.dismiss()
+                    Log.e("", e.message.toString())
+                    showToast("Failed to recognize text due to ${e.message}")
+                }
+        } catch (e: Exception) {
+            progressDialog.dismiss()
+            showToast("Failed to prepare image due to ${e.message}")
+        }
     }
 
-
+    fun showInputImageDialog() {
+        val popupMenu = PopupMenu(mActivity, mActivity.binding.Scan)
+        popupMenu.menu.add(Menu.NONE, 1, 1, "CAMERA")
+        popupMenu.menu.add(Menu.NONE, 2, 2, "GALLERY")
+        popupMenu.show()
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            val id = menuItem.itemId
+            if (id == 1) {
+                if (checkCameraPermission()) {
+                    pickImageCamera()
+                } else {
+                    requestCameraPermission()
+                }
+            } else if (id == 2) {
+                if (checkGalleryPermission()) {
+                    pickImageGallery()
+                } else {
+                    requestGalleryPermission()
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
+    }
+    
 
 }
